49fbe4b416c42a6b8f1f27085bc9d11df96c06aa
vijay yadav <124396738+vjyadav12@users.noreply.github.com>
2025-08-07 12:32:15 +0530

    backend logics

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ import express from 'express'
 import morgan from 'morgan'
 import userRouter from './Routers/UserRouter.js';
 import DbConnection from './DbConnection/DBconnetction.js';
+import cors from 'cors'
 
 const app = express()
 
 DbConnection()
 
+app.use(cors())
 app.use(express.json()) 
 app.use(express.urlencoded({extended:true}))
 app.use(morgan('dev'));
